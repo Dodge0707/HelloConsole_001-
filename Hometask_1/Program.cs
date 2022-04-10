9e5a8614d0797d78f4252bd3c7b5b68cea1f19b2
«Vova <vrobskiy@gmail.com>
2022-04-10 19:04:25 +0300

    add hometask Two friends

---
+++ Hometask_1/Program.cs
@@ -0,0 +1,27 @@
+﻿int firstFriend = 1;
+int secondFriend = 2;
+int dogspeed = 5;
+int count = 0;
+int d = 10000; 
+
+
+while (d > 10)
+{
+    int time = 0;
+    int friend = 1;
+    
+    if (friend == 1)
+    {
+        time = d / (firstFriend + dogspeed);
+        friend = 2;
+    } else
+    {
+        time = d / (secondFriend + dogspeed);
+        friend = 1;
+    }
+
+    d = d - (firstFriend + secondFriend) * time;
+    count ++;
+}
+
+Console.WriteLine(count);
